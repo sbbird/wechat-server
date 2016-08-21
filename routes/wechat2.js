eb002555696a1202f5a72d31cea58fd75eb7916a
Yiqing Zhu <sbbird.zhu@gmail.com>
2016-08-21 19:24:54 +0900

    抽奖

--- routes/wechat2.js
+++ routes/wechat2.js
@@ -10,7 +10,22 @@ var config = {
   encodingAESKey: 'Q2rGiDCetH0eN9E3afoeggSYzwdBnh1ogFtJhg8RsVk'
 };
 
+var answers = {};
+
 module.exports = wechat(config, function (req, res, next) {
   var message = req.weixin;
-  res.reply('hehe');
+
+  var content = message.content;
+  var user = message.OpenID;
+  if (message.match("抽奖")) {
+    isGood(user) ? res.reply('恭喜') : res.reply('谢谢参与');
+  } else {
+    res.reply('hehe');
+  }
 });
+
+function isGood(theUser) {
+  answers[theUser] = answers[theUser] || { count: 0 };
+  answers[theUser]['count']++;
+  return (answers[theUser]['count'] % 3 === 0);
+}
